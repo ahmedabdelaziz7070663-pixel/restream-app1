04831d7b39a94cbbb62ca96a0125d03dc74605d4
ahmedabdelaziz7070663-pixel <ahmedabdelaziz7070663@gmail.com>
2026-06-13 23:37:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const channels = {
   },
 
   ch2: {
-    input: "http://rgkkw.live:8080/live/akheelasharaf/97430689947/744518.ts",
+    input: "http://rgkkw.live/live/akheelasharaf/97430689947/744524.ts",
     output: "rtmp://rtmp.livepeer.com/live/0eef-2sbx-m34e-i0jf"
   },
 
