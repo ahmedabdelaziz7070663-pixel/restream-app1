2652de6bafa2108ffddd2845f9b383bc1c59587a
ahmedabdelaziz7070663-pixel <ahmedabdelaziz7070663@gmail.com>
2026-06-14 13:13:15 +0300

    Update input URL for ch5 configuration

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const channels = {
   },
 
   ch5: {
-    input: "http://rgkkw.live/live/akheelasharaf/97430689947/744527.ts",
+    input: "http://185.160.192.14/live/171348492752/5S6HGsea3j/255225.m3u8",
     output: "rtmp://rtmp.livepeer.com/live/a4be-dmef-x7d9-4kme"
   }
 };
