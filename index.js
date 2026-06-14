0426d74eb7d9b624f930e0918626a8f3d3303447
ahmedabdelaziz7070663-pixel <ahmedabdelaziz7070663@gmail.com>
2026-06-15 00:47:45 +0300

    Update input URL for channel 2

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const channels = {
   },
 
   ch2: {
-    input: "http://185.160.192.14/live/171348492752/5S6HGsea3j/255225.m3u8",
+    input: "http://rgkkw.live/live/akheelasharaf/97430689947/744524.ts",
     output: "rtmp://rtmp.livepeer.com/live/6a40-x3zs-p34r-ueay"
   },
 
